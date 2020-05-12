57dda46bf971174fcdc874b54d494d7f3f182817
David Rygwelski <drygw01@gmail.com>
2020-05-12 11:11:03 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_interacting_with_the_console
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
